a4156cd3d0e232c7718a40226b04d76ef3e7bb1a
Cristian <seo2@modosantiago.com>
2018-04-08 21:28:59 -0300

    front

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/despierta.css">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png?v=1" />
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -18,6 +21,7 @@
     <![endif]-->
 
 	<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117158293-1"></script>
@@ -31,50 +35,129 @@
 
 </head>
 <body>
-	
-	 <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="../navbar/">Default</a></li>
-            <li><a href="../navbar-static-top/">Static top</a></li>
-            <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-	
-    <div class="container">
-    	
-    </div>
-    
+	<header>
+		 <!-- Fixed navbar -->
+	    <nav class="navbar navbar-default navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#"><img src="assets/img/logo-despierta.png" ></a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li><a href="../navbar/">Inicio</a></li>
+	            <li><a href="../navbar-static-top/">Historias</a></li>
+	            <li class="active"><a href="./">Participa por entradas <span class="sr-only">(current)</span></a></li>
+	            <li><a href="../navbar-static-top/">Galería</a></li>
+	            <li><a href="../navbar-static-top/"><i class="fab fa-facebook-f"></i></a></li>
+	            <li><a href="../navbar-static-top/"><i class="fab fa-instagram"></i></a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+	</header>
+	<section id="barra">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<img src="assets/img/logo-seguros.png" id="logo-seguros">
+				</div>
+				<div class="col-sm-6 text-right">
+					<img src="assets/img/logo-sura.png" id="logo-sura">
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="slider-principal">
+		<img src="assets/img/slider1.jpg" class="img-responsive">
+	</section>
+	<section id="historias">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-4">
+					<img src="assets/img/tit-conoce.png" class="img-responsive">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12">
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="eventos">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<img src="assets/img/tit-eventos.png" class="img-responsive">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-12 text-center">
+					<p>Conoce la cartelera completa ingresando aquí</p>
+					<a href="javascript:void(0);" class="btn btn-primary">Cartelera eventos</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="participa">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<img src="assets/img/tit-participa.png" class="img-responsive">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="galeria">
+		<div class="container">
+			<div class="row">
+				
+			</div>
+		</div>
+	</section>
+    <footer>
+	    <div class="container">
+	    	<div class="row">
+				    <nav class="navbar navbar-default">
+				      <div class="container">
+				        <div class="navbar-header">
+				          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				            <span class="sr-only">Toggle navigation</span>
+				            <span class="icon-bar"></span>
+				            <span class="icon-bar"></span>
+				            <span class="icon-bar"></span>
+				          </button>
+				          <a class="navbar-brand" href="#"><img src="assets/img/logo-despierta-blanco.png" ></a>
+				        </div>
+				        <div id="navbar" class="navbar-collapse collapse">
+				          <ul class="nav navbar-nav navbar-right">
+				            <li><a href="../navbar/">Inicio</a></li>
+				            <li><a href="../navbar-static-top/">Historias</a></li>
+				            <li class="active"><a href="./">Participa por entradas <span class="sr-only">(current)</span></a></li>
+				            <li><a href="../navbar-static-top/">Galería</a></li>
+				            <li><a href="../navbar-static-top/"><i class="fab fa-facebook-f"></i></a></li>
+				            <li><a href="../navbar-static-top/"><i class="fab fa-instagram"></i></a></li>
+				          </ul>
+				        </div><!--/.nav-collapse -->
+				      </div>
+				    </nav>
+	    	</div>
+	    	<div class="row text-right">
+	    		<p class="copyright">© 2018 Todos los Derechos Reservados. Seguros Sura.</p>
+	    	</div>
+	    </div>
+    </footer>
     
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
